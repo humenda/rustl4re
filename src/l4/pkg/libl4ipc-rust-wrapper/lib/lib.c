eeceb21f16b1398d4c4dae8e01b093cb3ec6bb18
Sebastian Humenda <shumenda@gmx.de>
2018-03-02 13:47:47 +0100

    add libl4ipc-rust-wrapper + further implement trust
    -   trust (the literal examples/sys/ipc port) no has the rough structure
        in place and sends IPC messages
    -   libipc-rust now depends on a small intermediate wrapper exposing
    normally C-inline functions as a thin C-function with a address to jump
    to from Rust's side
        -   layer is as thin as possible: all inlined, except for the one
        real C function call

---
+++ src/l4/pkg/libl4ipc-rust-wrapper/lib/lib.c
@@ -0,0 +1,35 @@
+#include <pthread-l4.h>
+#include <l4/libl4ipc-rust-wrapper/lib.h>
+
+l4_msg_regs_t *l4_utcb_mr_w() {
+    return l4_utcb_mr();
+}
+
+l4_msgtag_t l4_ipc_call_w(l4_cap_idx_t object,
+        l4_utcb_t *utcb, l4_msgtag_t tag,
+        l4_timeout_t timeout) {
+    return l4_ipc_call(object, utcb, tag, timeout);
+}
+
+l4_umword_t l4_ipc_error_w(l4_msgtag_t tag, l4_utcb_t *utcb) {
+    return l4_ipc_error(tag, utcb);
+}
+
+l4_msgtag_t l4_ipc_wait_w(l4_utcb_t *utcb, l4_umword_t *label,
+        l4_timeout_t timeout) {
+    return l4_ipc_wait(utcb, label, timeout);
+}
+l4_utcb_t *l4_utcb_w() {
+    return l4_utcb();
+}
+
+l4_msgtag_t l4_msgtag_w(long label, unsigned words, unsigned items,
+        unsigned flags) {
+    return l4_msgtag(label, words, items, flags);
+}
+
+l4_msgtag_t l4_ipc_reply_and_wait_w(l4_utcb_t *utcb, l4_msgtag_t tag,
+        l4_umword_t *src, l4_timeout_t timeout) {
+    return l4_ipc_reply_and_wait(utcb, tag, src, timeout);
+}
+
