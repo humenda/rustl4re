c1b022f28135c405db68fbe2ff742e1bc8edcfed
Sebastian Humenda <shumenda@gmx.de>
2018-05-29 17:51:12 +0200

    add partially finished server

--- src/l4/pkg/l4re-rust/cap/cap-rs-sys/lib.rs
+++ src/l4/pkg/l4re-rust/cap/cap-rs-sys/lib.rs
@@ -4,12 +4,16 @@
 // ToDo: get rid of these
 #![feature(pointer_methods)]
 
-#![feature(asm)]
 
 use std::{ffi::CString,
         os::raw::{c_uchar, c_uint}};
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+////////////////////////////////////////////////////////////////////////////////
+// redefined constants
+
+pub const FPAGE_RWX: u8 = L4_fpage_rights_L4_FPAGE_RWX as u8;
 
 ////////////////////////////////////////////////////////////////////////////////
 // inline functions from l4/sys/env.h:
@@ -28,7 +32,44 @@ pub fn l4_is_invalid_cap(cap: l4_cap_idx_t) -> bool {
 
 #[inline]
 pub unsafe fn l4_obj_fpage(obj: l4_cap_idx_t, order: c_uint, rights: c_uchar)
-        -> l4_fpage_t
-{
+        -> l4_fpage_t {
     l4_obj_fpage_w(obj, order, rights)
 }
+
+////////////////////////////////////////////////////////////////////////////////
+// complete re-implementations
+
+#[inline]
+pub unsafe fn l4re_env() -> *mut l4re_env_t {
+    l4re_global_env
+}
+
+/// Create the first word for a map item for the memory space.
+///
+/// Returns the value to be used as first word in a map item for memory.
+///
+/// * n`spot`   Hot spot address, used to determine what is actually mapped when send and receive
+///     flex page have differing sizes.
+/// * `cache`  Cacheability hints for memory flex pages. See `l4_fpage_cacheability_opt_t`
+/// * `grant`  Indicates if it is a map or a grant item.
+#[inline]
+pub fn l4_map_control(snd_base: l4_umword_t, cache: u8, grant: u64)
+        -> l4_umword_t {
+    (snd_base & L4_fpage_control_L4_FPAGE_CONTROL_MASK)
+        | ((cache as l4_umword_t) << 4)
+        | (l4_msg_item_consts_t_L4_ITEM_MAP as u64)
+        | grant
+}
+
+
+/// Create the first word for a map item for the object space.
+///
+/// Returns The value to be used as first word in a map item for kernel objects
+/// or IO-ports.
+/// *   `spot`:   Hot spot address, used to determine what is actually mapped
+///     when send and receive flex pages have different size.
+/// *   `grant`:  Indicates if it is a map item or a grant item.
+pub fn l4_map_obj_control(snd_base: l4_umword_t, grant: u64) -> l4_umword_t {
+    l4_map_control(snd_base, 0, grant)
+}
+
--- src/l4/pkg/l4re-rust/libl4re-wrapper/lib/ipc.c
+++ src/l4/pkg/l4re-rust/libl4re-wrapper/lib/ipc.c
@@ -28,6 +28,12 @@ EXTERN l4_msgtag_t l4_msgtag_w(long label, unsigned words, unsigned items,
     return l4_msgtag(label, words, items, flags);
 }
 
+EXTERN l4_msgtag_t l4_ipc_receive_w(l4_cap_idx_t object, l4_utcb_t *utcb,
+        l4_timeout_t timeout) {
+    return l4_ipc_receive(object, utcb, timeout);
+}
+
+
 EXTERN l4_msgtag_t l4_ipc_reply_and_wait_w(l4_utcb_t *utcb, l4_msgtag_t tag,
         l4_umword_t *src, l4_timeout_t timeout) {
     return l4_ipc_reply_and_wait(utcb, tag, src, timeout);
