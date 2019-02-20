b6999db9eb6b473a58c73be0909d18e467f572db
Sebastian Humenda <shumenda@gmx.de>
2019-02-20 12:56:43 +0100

    l4_derive: proper error handling, restructuring; documentation
    -   l4_derive/error handling: now state-of-the-art with helpful messages
    -   add docs for 4_derive
    -   restructure l4_derive

--- src/l4/pkg/l4rust/l4-rust/l4_derive-rust/macros.rs
+++ src/l4/pkg/l4rust/l4-rust/l4_derive-rust/macros.rs
@@ -1,7 +1,19 @@
 macro_rules! err {
+    // something that implements spanned + an error message
     ($obj_with_span:expr, $msg:literal) => {{
         $obj_with_span.span().unstable().error($msg).emit();
         return Err(syn::Error::new($obj_with_span.span(), $msg));
-    }}
+    }};
+
+    ($msg:literal) => {
+        compile_error!($msg);
+    }
 }
 
+macro_rules! raw_type {
+    ($($raw:tt)*) => {
+        syn::Type::Verbatim(syn::TypeVerbatim {
+                tts: TokenStream::from_str($($raw)*).unwrap() 
+        })
+    }
+}
