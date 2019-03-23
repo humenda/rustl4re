17c00cd6215ec260552b337046affb6c69815cb0
Sebastian Humenda <shumenda@gmx.de>
2019-03-23 19:15:46 +0100

    add simple C++ bench1-primitive test

---
+++ src/l4/pkg/oxisamples/bench/bench1-primitive_cc/client.cc
@@ -0,0 +1,54 @@
+#include <l4/sys/err.h>
+#include <l4/sys/types.h>
+#include <l4/re/env>
+#include <l4/re/util/cap_alloc>
+#include <l4/util/rdtsc.h>
+#include <algorithm>
+#include <vector>
+
+#include <stdio.h>
+#include "../interface.h"
+
+using std::max_element;
+using std::min_element;
+
+int main() {
+    L4::Cap<Bencher> server = L4Re::Env::env()->get_cap<Bencher>("channel");
+    if (!server.is_valid()) {
+        printf("Could not get server capability!\n");
+        return 1;
+    }
+
+    l4_uint32_t val1 = 8;
+    l4_uint32_t val2 = 5;
+    l4_int32_t res = 0;
+    std::vector<unsigned long> v;
+    v.reserve(1000);
+
+    printf("[cc] Starting primitive subtraction test\n");
+
+    for (unsigned int i = 0; i < 1000; i++) {
+        auto start = l4_rdtsc();
+#ifndef TEST_STRING
+        if (server->sub(val1, val2, &res)) {
+#else
+            // ToDo: not functional
+        if (server->strpingpong(....)) {
+#endif
+            printf("Error talking to server\n");
+            return 1;
+        }
+        auto end = l4_rdtsc();
+        v.push_back(end - start);
+    }
+    std::sort(v.begin(), v.end());
+
+    printf("min: %lu\n", *min_element(std::begin(v), std::end(v)));
+    printf("max: %lu\n", *max_element(std::begin(v), std::end(v)));
+    int sum = 0;
+    for (unsigned int i = 0; i < (unsigned int)v.size(); sum += v[i]);
+    printf("avg: %lu\n", sum / v.size());
+    printf("median: %lu\n", v[500]);
+
+    return 0;
+}
---
+++ src/l4/pkg/oxisamples/bench/bench1-primitive_cc/server.cc
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <l4/re/env>
+#include <l4/re/util/cap_alloc>
+#include <l4/re/util/object_registry>
+#include <l4/re/util/br_manager>
+#include <l4/sys/cxx/ipc_epiface>
+
+#include "../interface.h"
+
+using L4::Ipc::String;
+
+static L4Re::Util::Registry_server<L4Re::Util::Br_manager_hooks> server;
+
+class Bench_server : public L4::Epiface_t<Bench_server, Bencher>
+{
+public:
+  int op_sub(Bencher::Rights, l4_uint32_t a, l4_uint32_t b, l4_int32_t &res) {
+    res = (l4_int32_t)a - b;
+    return 0;
+  }
+
+#ifdef TEST_STRING
+  int op_strpingpong(Bencher::Rights, L4::Ipc::Array_in_buf<char, unsigned long> const &s, String<> *a) {
+      (void)s; (void)a;
+    //a->copy_in(s.data);
+    return 0;
+  }
+#endif
+};
+
+
+int
+main()
+{
+  static Bench_server bench;
+
+  // Register Bench server
+  if (!server.registry()->register_obj(&bench, "channel").is_valid())
+    {
+      printf("Could not register my service, is there a 'channel' in the caps table?\n");
+      return 1;
+    }
+
+  // Wait for client requests
+  server.loop();
+
+  return 0;
+}
