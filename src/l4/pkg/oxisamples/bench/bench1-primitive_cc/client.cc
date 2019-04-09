e90d0c7fbc95a7b1916d04ea6decb112254692f9
Sebastian Humenda <shumenda@gmx.de>
2019-04-09 12:34:04 +0200

    extend bench*_cc to properly format all measured IPC benchmarking results

--- src/l4/pkg/oxisamples/bench/bench1-primitive_cc/client.cc
+++ src/l4/pkg/oxisamples/bench/bench1-primitive_cc/client.cc
@@ -13,6 +13,20 @@ using std::max_element;
 using std::min_element;
 
 const char* MY_MESSAGE = "Premature optimization is the root of all evil. Yet we should not pass up our opportunities in that critical 3%.";
+
+#ifdef BENCH_SERIALISATION
+void format_min_median_max(const char* msg, l4_int64_t f(ClientCall));
+void format_min_median_max(const char* msg, l4_int64_t f(ClientCall)) {
+    auto v = std::vector<l4_int64_t>();
+    for (l4_uint64_t i = 0; i < MEASURE_RUNS; i++) {
+        v.push_back(f(CLIENT_MEASUREMENTS[i]));
+    }
+   std::sort(v.begin(), v.end());
+    printf("%-25s%-10lli%-10lli%-10lli\n", msg, v[0], v[MEASURE_RUNS/2],
+            v.back());
+}
+#endif
+
 int main() {
     L4::Cap<Bencher> server = L4Re::Env::env()->get_cap<Bencher>("channel");
     if (!server.is_valid()) {
@@ -32,14 +46,14 @@ int main() {
     cycles.reserve(100000);
 
 
-    for (unsigned int i = 0; i < 100000; i++) {
+    for (unsigned int i = 0; i < MEASURE_RUNS; i++) {
+        auto start = l4_rdtsc();
 #ifdef TEST_STRING
         L4::Ipc::String<const char> knuth = L4::Ipc::String<>(MY_MESSAGE);
         auto out = L4::Ipc::String<char>((char *)malloc((knuth.length + 1) * sizeof(char)));
 #endif
-        auto start = l4_rdtsc();
 #ifndef TEST_STRING
-        if (server->sub(val1, val2, &res))
+        if (server->sub(val1, val2, res))
 #else
         if (server->strpingpong(knuth, out))
 #endif
@@ -56,14 +70,30 @@ int main() {
         free(out.data);
 #endif
     }
+    printf("srl := serialisation\n");
+    printf("%-25s%-10s%-10s%-10s\n", "Value", "Min", "Median", "Max");
     std::sort(cycles.begin(), cycles.end());
 
-    printf("min: %lu\n", *min_element(std::begin(cycles), std::end(cycles)));
-    printf("max: %lu\n", *max_element(std::begin(cycles), std::end(cycles)));
-    int sum = 0;
-    for (unsigned int i = 0; i < (unsigned int)cycles.size(); i++, sum += cycles[i]);
-    printf("avg: %lu\n", sum / cycles.size());
-    printf("median: %lu\n", cycles[500]);
+    printf("%-25s%-10lu%-10lu%-10lu\n", "Global",
+            *min_element(std::begin(cycles), std::end(cycles)),
+            cycles[MEASURE_RUNS / 2],
+            *max_element(std::begin(cycles), std::end(cycles)));
+    // client argument serialisation duration
+    format_min_median_max("client argument srl", [](ClientCall c) {
+        return c.arg_srl_end - c.arg_srl_start;
+    });
+    format_min_median_max("arg srl -> IPC call", [](ClientCall c) {
+        return c.call_start - c.arg_srl_end;
+    });
+    format_min_median_max("arg srl -> IPC call", [](ClientCall c) {
+        return c.call_start - c.arg_srl_end;
+    });
+    format_min_median_max("IPC call", [](ClientCall c) {
+        return c.ret_desrl_start - c.call_start;
+    });
+    format_min_median_max("return value desrl", [](ClientCall c) {
+        return c.ret_desrl_end - c.ret_desrl_start;
+    });
 
     return 0;
 }
