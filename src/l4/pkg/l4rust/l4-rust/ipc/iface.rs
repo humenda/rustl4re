1cc79d1095bed8c6f2358d6edb293391a8599d55
Sebastian Humenda <shumenda@gmx.de>
2019-04-09 21:40:18 +0200

    ipc/iface.rs: simplify method calls of Serialiser trait

--- src/l4/pkg/l4rust/l4-rust/ipc/iface.rs
+++ src/l4/pkg/l4rust/l4-rust/ipc/iface.rs
@@ -187,11 +187,11 @@ macro_rules! iface_back {
                 $( // iterate over functions and op codes
                     $op_code => unsafe {
                         let user_ret = self.$name(
-                            $(<$argtype as $crate::ipc::Serialiser>::
+                            $(<$argtype>::
                                 read(mr, bufs)?),*
                         )?;
                         mr.reset(); // reset MRs, not the bufs (not used for sending)
-                        <$ret as $crate::ipc::Serialiser>::write(user_ret, mr)?;
+                        <$ret>::write(user_ret, mr)?;
                         // on return, label/protocol is 0 for success and a custom error code
                         // otherwise; negative means some IPC failure
                         Ok($crate::ipc::MsgTag::new(0,
