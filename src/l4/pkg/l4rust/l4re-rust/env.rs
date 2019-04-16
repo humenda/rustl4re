ab5b8996e62781b685fe388b15e64cf79caeeed9
Sebastian Humenda <shumenda@gmx.de>
2019-04-16 10:21:49 +0200

    l4re-rust: change memory allocation interface
    -   MemAlloc is a fake C++ interface and thus didn't work until now, it
        is not provided until its façade has been rewritten
    -   add a mem_alloc function using the default allocator from the L4Re
    environment

--- src/l4/pkg/l4rust/l4re-rust/env.rs
+++ src/l4/pkg/l4rust/l4re-rust/env.rs
@@ -1,6 +1,20 @@
 use l4::cap::{Cap, IfaceInit};
-use crate::sys::l4re_env_get_cap;
+use crate::sys::{l4re_env_get_cap, l4re_env};
 
+/// Retrieve a capability from the initial capability set of a L4Re task
 pub fn get_cap<T: IfaceInit>(name: &str) -> Option<Cap<T>> {
     l4re_env_get_cap(name).map(|cap_idx| Cap::<T>::new(cap_idx))
 }
+
+/*
+/// Retrieves the initial memory allocator
+///
+/// Each L4Re task comes with an initial memory allocator by which the task can
+/// request more memory. This function gets the capability to it.
+#[inline]
+pub fn mem_alloc() -> l4::cap::Cap<MemAlloc> {
+    unsafe { // pointer is always valid
+        l4::cap::Cap::<MemAlloc>::new((*l4re_env()).mem_alloc)
+    }
+}
+*/
