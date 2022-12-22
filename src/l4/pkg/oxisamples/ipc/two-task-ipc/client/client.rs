67ded6326d7116e1bebbab7b77aabbf9d578ff1a
Sebastian Humenda <sebastian.humenda@huawei.com>
2022-12-22 16:25:33 +0100

    [WIP] untested fix for the two-task server IPC port

--- src/l4/pkg/oxisamples/ipc/two-task-ipc/client/client.rs
+++ src/l4/pkg/oxisamples/ipc/two-task-ipc/client/client.rs
@@ -1,8 +1,8 @@
-extern crate l4_sys as l4;
-extern crate l4re;
-
-use l4::{l4_utcb, l4_msgtag};
-use std::{thread, time};
+#![no_std]
+use l4::{
+    ipc::{call, sleep, MsgTag},
+    sys::{l4_msgtag, l4_timeout_t, l4_utcb},
+};
 
 pub fn main() {
     // retrieve IPC gate from Ned (created in the *.cfg script file
@@ -22,23 +22,22 @@ pub fn main() {
         // the message tag contains instructions to the kernel and to the other party what is being
         // transfered: no protocol, 1 message register, no flex page and no flags; flex pages are
         // not relevant here
-        unsafe {
-            let send_tag = l4_msgtag(0, 1, 0, 0);
-            let tag = l4::l4_ipc_call(server, l4_utcb(), send_tag,
-                    l4::l4_timeout_t { raw: 0 });
-            println!("data sent");
-            // check for IPC error, if yes, print out the IPC error code, if not, print the received
-            // result.
-            match l4::l4_ipc_error(tag, l4_utcb()) {
-                0 => // success
-                    println!("Received: {}\n", (*l4::l4_utcb_mr()).mr[0]),
-                ipc_error => println!("client: IPC error: {}\n",  ipc_error),
-            };
-        }
+        let send_tag = MsgTag::new(0, 1, 0, 0);
+        let utcb = Utcb::current();
+        let tag = call(server, &utcb, send_tag, l4_timeout_t { raw: 0 });
+        println!("data sent");
+        // check for IPC error, if yes, print out the IPC error code, if not, print the received
+        // result.
+        match l4::l4_ipc_error(tag, l4_utcb()) {
+            0 =>
+            // success
+            {
+                println!("Received: {}\n", (*l4::l4_utcb_mr()).mr[0])
+            }
+            ipc_error => println!("client: IPC error: {}\n", ipc_error),
+        };
 
-        thread::sleep(time::Duration::from_millis(1000));
+        sleep(l4_timeout_t { raw: 200 });
         counter += 1;
     }
 }
-
-
--- src/l4/pkg/oxisamples/ipc/two-task-ipc/server/server.rs
+++ src/l4/pkg/oxisamples/ipc/two-task-ipc/server/server.rs
@@ -1,67 +1,90 @@
 //! A simple square server (squaring a number for a client). This server listens on an IPC gate for
 //! incoming connections of arbitrary clients.
-// This doesn't work with stable Rust, use "extern crate libc" here:
-#![feature(libc)]
-extern crate l4_sys as l4;
-extern crate l4re;
-extern crate libc;
 
-use l4::{l4_ipc_error, l4_msgtag, l4_utcb};
+#![no_std]
 
+use l4::{
+    println,
+    sys::{l4_ipc_error, l4_msgtag},
+};
+
+#[cfg(feature = "std")]
 pub fn main() {
-    unsafe { // avoid deep nesting of unsafe blocks, just make the whole program unsafe
-        unsafe_main();
-    }
+    main_body();
 }
 
-pub unsafe fn unsafe_main() {
+#[cfg(not(feature = "std"))]
+pub extern "C" fn main(_a: u32, _ptr: *const u8) {
+    main_body();
+}
+
+pub fn main_body() {
     // IPC gates can receive from multiple senders, hence messages need an idification label for
     // clients. Here's one:
     let gatelabel = 0b11111100 as u64;
     // place to put label in when a message is received
-    let mut label = std::mem::uninitialized();
-
+    let mut label = 0;
 
     // get IPC gate capability from Lua script (see ../*.cfg)
-    let gate = l4re::sys::l4re_env_get_cap("channel").unwrap();
-    // check whether we got something
-    if l4::l4_is_invalid_cap(gate) {
+    let gate = 0; // ToDounsafe { l4::sys::l4re_env_get_cap("channel").unwrap() };
+                  // check whether we got something
+    if l4::sys::l4_is_invalid_cap(gate) {
         panic!("No IPC Gate found.");
     }
 
     // bind IPC gate to main thread with custom label
-    match l4_ipc_error(l4::l4_rcv_ep_bind_thread(gate,
-            (*l4re::sys::l4re_env()).main_thread, gatelabel), l4_utcb()) {
+    match unsafe {
+        l4_ipc_error(
+            l4::sys::l4_rcv_ep_bind_thread(gate, (*l4re::sys::l4re_env()).main_thread, gatelabel),
+            l4::sys::l4_utcb(),
+        )
+    } {
         0 => (),
-        n => panic!("Error while binding IPC gate: {}", n)
+        n => panic!("Error while binding IPC gate: {}", n),
     };
     println!("IPC gate received and bound.");
 
     println!("waiting for incoming connections");
     // Wait for requests from any thread. No timeout, i.e. wait forever. Note that the label of the
     // sender will be placed in `label`.
-    let mut tag = l4::l4_ipc_wait(l4::l4_utcb(), &mut label,
-            l4::l4_timeout_t { raw: 0 });
+    let mut tag = unsafe {
+        l4::sys::l4_ipc_wait(
+            l4::sys::l4_utcb(),
+            &mut label,
+            l4::sys::l4_timeout_t { raw: 0 },
+        )
+    };
     loop {
         // Check if we had any IPC failure, if yes, print the error code and
         // just wait again.
-        let ipc_error = l4::l4_ipc_error(tag, l4::l4_utcb());
+        let ipc_error = unsafe { l4::sys::l4_ipc_error(tag, l4::sys::l4_utcb()) };
         if ipc_error != 0 {
             println!("server: IPC error: {}\n", ipc_error);
-            tag = l4::l4_ipc_wait(l4::l4_utcb(), &mut label, 
-                    l4::l4_timeout_t { raw: 0 });
+            tag = unsafe {
+                l4::sys::l4_ipc_wait(
+                    l4::sys::l4_utcb(),
+                    &mut label,
+                    l4::sys::l4_timeout_t { raw: 0 },
+                )
+            };
             continue;
         }
 
         // IPC was ok, now get value from message register 0 of UTCB and store the square of it
         // back to it
-        let val = (*l4::l4_utcb_mr()).mr[0];
-        (*l4::l4_utcb_mr()).mr[0] = val * val;
-        println!("new value: {}", (*l4::l4_utcb_mr()).mr[0]);
+        let val = unsafe { (*l4::sys::l4_utcb_mr()).mr[0] };
+        unsafe { (*l4::sys::l4_utcb_mr()).mr[0] = val * val };
+        println!("new value: {}", (*l4::sys::l4_utcb_mr()).mr[0]);
 
         // send reply and wait again for new messages; the message tag specifies to send one
         // machine word with no protocol, no flex pages and no flags
-        tag = l4::l4_ipc_reply_and_wait(l4::l4_utcb(), l4_msgtag(0, 1, 0, 0),
-                              &mut label, l4::l4_timeout_t { raw: 0 });
+        tag = unsafe {
+            l4::sys::l4_ipc_reply_and_wait(
+                l4::sys::l4_utcb(),
+                l4_msgtag(0, 1, 0, 0),
+                &mut label,
+                l4::sys::l4_timeout_t { raw: 0 },
+            )
+        };
     }
 }
