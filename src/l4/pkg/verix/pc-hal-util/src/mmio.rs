3813118bdf2263487d7dd338deae871f54668e89
Henrik <hargonix@gmail.com>
2023-07-26 17:56:00 +0200

    feat: WO MMIO in pc-hal-util

--- src/l4/pkg/verix/pc-hal-util/src/mmio.rs
+++ src/l4/pkg/verix/pc-hal-util/src/mmio.rs
@@ -50,6 +50,61 @@ macro_rules! reg_spec2types {
         }
     };
 
+    ($bar:ident, $reg:ident, $reg_addr:literal, WO, $($field:ident @ $end:literal : $start:literal),+) => {
+        pub mod $reg {
+            pub struct Register<'a, IM: pc_hal::traits::IoMem> {
+                bar: &'a mut super::$bar<IM>
+            }
+
+            pub struct W {
+                val: u32
+            }
+
+            impl W {
+                $(
+                    // TODO: This type can be improved
+                    pub fn $field(mut self, val: u32) -> Self {
+                        // There is probably a more clever way to calculate this?
+                        // I do however believe that LLVM should be capable of optimizing
+                        let mut mask = u32::MAX;
+                        mask = (mask >> $start) << $start; // Get rid of the first n bits
+                        let help = 31 - $end;
+                        mask = (mask << help) >> help; // Get rid of the last m bits
+                        let mut shifted_val = val << $start;
+                        // TODO: we could have an assertion here that checks that
+                        // shifted_val & mask = shifted_val
+                        // which would be the definition of a correct val
+                        // this could be a verification condition for kani
+                        shifted_val = shifted_val & mask;
+                        self.val &= !mask; // clear all bits of the field that we have so far
+                        self.val |= shifted_val; // set the new bits
+                        self
+                    }
+                )+
+            }
+
+
+            impl<'a, IM: pc_hal::traits::IoMem> Register<'a, IM> {
+                pub fn write<F>(&mut self, f: F)
+                where
+                    F: FnOnce(W) -> W
+                {
+                    let w = W { val : 0 };
+                    let w_final = f(w);
+                    self.bar.mem.write32($reg_addr, w_final.val);
+                }
+            }
+
+            impl<IM: pc_hal::traits::IoMem> super::$bar<IM> {
+                pub fn $reg<'a>(&'a mut self) -> Register<'a, IM> {
+                    Register {
+                        bar: self
+                    }
+                }
+            }
+        }
+    };
+
     ($bar:ident, $reg:ident, $reg_addr:literal, RW, $($field:ident @ $end:literal : $start:literal = $default:literal),+) => {
         pub mod $reg {
             pub struct Register<'a, IM: pc_hal::traits::IoMem> {
@@ -88,7 +143,6 @@ macro_rules! reg_spec2types {
             impl W {
                 $(
                     // TODO: This type can be improved
-                    // TODO: test this impl
                     pub fn $field(mut self, val: u32) -> Self {
                         // There is probably a more clever way to calculate this?
                         // I do however believe that LLVM should be capable of optimizing
@@ -159,11 +213,6 @@ macro_rules! reg_specs2types {
     ($bar:ident, $reg:ident @ $reg_addr:literal $perms:ident { $($descrs:tt)+ }) => {
         reg_spec2types!($bar, $reg, $reg_addr, $perms, $($descrs)+); 
     };
-/*
-    // TODO: reset API if we need it
-    ($reg:ident @ $reg_addr:literal RW { $($field:ident @ $end:literal : $start:literal = $default:literal),+ }, $bar:ident) => {
-    };
-*/
 }
 
 #[macro_export]
